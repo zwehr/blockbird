9455f8950f3fd1b2dbe1f62d7f1d6b4136b161ec
zwehr <zwehrmeister@gmail.com>
2017-04-19 00:41:57 -0400

    add collision detection

--- brains.js
+++ brains.js
@@ -1,16 +1,32 @@
 var canvas;
 var canvasContext;
 
+var isPaused = false;
+
 var score = 0;
 
 var birdY = 300;
 var wallX = 380;
 
+var topWallY = 0;
+var bottomWallY = 400;
+
+var wallWidth = 20;
+var birdWidthAndHeight = 20;
+
+var topWallHeight = 200;
+var bottomWallHeight = 200;
+
 window.onload = function() {
     canvas = document.getElementById('gameCanvas');
     canvasContext = canvas.getContext('2d');
-    //re-draw every 10 ms
-    setInterval(drawAll, 10);
+    //move objects and re-draw every 20 ms
+        setInterval(function() {
+            if (!isPaused) {
+            drawAll();
+            moveAll();
+            checkForCollision();
+        }}, 20);
 }
         
 function drawAll() {
@@ -18,21 +34,33 @@ function drawAll() {
     canvasContext.fillStyle = 'black';
     canvasContext.fillRect(0, 0, canvas.width, canvas.height);
     canvasContext.fillStyle = 'white';
-    canvasContext.fillRect(wallX, 0, 20, 200);
+    canvasContext.fillRect(wallX, topWallY, wallWidth, topWallHeight);
     canvasContext.fillStyle = 'white';
-    canvasContext.fillRect(wallX, 400, 20, 250);
+    canvasContext.fillRect(wallX, bottomWallY, wallWidth, bottomWallHeight);
     canvasContext.fillStyle = 'red';
-    canvasContext.fillRect(120, birdY, 20, 20);
+    canvasContext.fillRect(120, birdY, birdWidthAndHeight, birdWidthAndHeight);
     canvasContext.fillStyle = 'green';
     canvasContext.font = '24px serif';
     canvasContext.fillText('Score: ' + score, 10, 30);
     
+    if (checkForCollision()) {
+        console.log("collision");
+        isPaused = true;
+    }
+}
+
+function moveAll() {
+    //make bird "jump" with click or Up key
+    document.onclick = function() {
+        birdY -= 100;
+    }
+    
     //move bird down and wall left at all times
-    birdY += 2;
+    birdY += 3;
     wallX--;
     
     //reset wall
-    if (wallX == -20) {
+    if (wallX < -20) {
         wallX = 380;
     }
     
@@ -42,7 +70,20 @@ function drawAll() {
     }
 }
 
-//make bird "jump" with click
-document.onclick = function() {
-    birdY -= 100;
+function checkForCollision() {
+    //check to make sure bird is between top and bottom wall
+    while (wallX < 140 && wallX > 100) {
+        if (birdY < (topWallY + topWallHeight) || birdY > bottomWallY + birdWidthAndHeight ) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    //check to make sure bird is above bottom of canvas
+    if (birdY > canvas.height - birdWidthAndHeight) {
+        return true;
+    } else 
+        return false;
+    
 }
